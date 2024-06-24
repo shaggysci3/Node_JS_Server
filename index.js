1db97eafdce9babcfa7f76227d0e2392b96a6491
matthew <shaggysci3@gmail.com>
2024-06-24 11:10:09 -0400

    added cors for the local host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http').createServer();
 
 
 const io = require('socket.io')(http,{
-    cors:{origin:"*"}
+    cors:{origin:"http://127.0.0.1:5173/cart"}
 });
 
 io.on('connection', (socket)=>{
